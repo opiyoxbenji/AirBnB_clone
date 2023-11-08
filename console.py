6844439aa37afd6709f4fd97e255ee3caab17d9f
pixelbushi <opiyosbenji@gmail.com>
2023-11-08 20:25:43 +0300

    first steps for the console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
