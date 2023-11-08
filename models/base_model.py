90a65e4909e1b5b09d6851088ee6dbe542bc4e78
Colin <clnearle@gmail.com>
2023-11-08 01:29:02 -0800

    updated base model for a dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,21 @@ class BaseModel:
     """
     base class with attributes for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initialize attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """
