192abcd59226163b9f3d39b02e5a7f785a0c4d08
Colin <clnearle@gmail.com>
2023-11-08 10:31:47 -0800

    updated the console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import cmd
-
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -16,6 +17,41 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered"""
         pass
+
+    def do_create(self, arg):
+        """
+        creates new instance of basemodel
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        prints the string representation of instance
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in storage.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        if key in storage.all():
+            print(storage.all()[key])
+        else
+        print("** no instance found **")
 
 
 if __name__ == '__main__':
